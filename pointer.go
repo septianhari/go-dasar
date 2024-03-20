0fd3ba4d75aee73b63b7fcbcebcd440f6a35dc48
septianhari <aribarno86@gmail.com>
2024-03-20 16:41:02 +0700

    lat pointer

--- pointer.go
+++ pointer.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-
 	var number = 10
+	var pointer *int = &number // Mengambil alamat dari variabel number
 
-	fmt.Println(number)
+	*pointer = 20 // Mengubah nilai yang ditunjuk oleh pointer
 
+	fmt.Println("number:", number)
+	fmt.Println("pointer:", *pointer) // Mencetak nilai yang ditunjuk oleh pointer
 }
