2b75f65cbada3157213eb77b0f0cd15adbaf5982
septianhari <aribarno86@gmail.com>
2024-03-25 22:34:25 +0700

    :rice: commit

---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var age uint
+	// age = 30
+	// fmt.Println(age)
+
+	nama, umur, negara := "Har", 22, "Indonesia"
+	fmt.Println(nama)
+	fmt.Println(umur)
+	fmt.Println(negara)
+
+	var isOnline bool
+
+	isOnline = true
+	fmt.Println(isOnline)
+}
