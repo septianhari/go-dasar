a37e662120f814205b8dfe8047835fac98543972
septianhari <aribarno86@gmail.com>
2024-03-31 14:18:19 +0700

    switch case

---
+++ switch.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	day := 1
+
+	switch day {
+	case 1:
+		fmt.Println("Senin")
+	case 2:
+		fmt.Println("Selasa")
+	case 3:
+		fmt.Println("Rabu")
+	case 4:
+		fmt.Println("Kamis")
+	case 5:
+		fmt.Println("Jumat")
+	case 6:
+		fmt.Println("Sabtu")
+	case 7:
+		fmt.Println("Minggu")
+
+	}
+
+}
