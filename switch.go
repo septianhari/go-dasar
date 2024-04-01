10a579a339a94a7cc9edbcfd09e0304cc6c56231
septianhari <aribarno86@gmail.com>
2024-04-01 23:48:00 +0700

    mencoba consisten

--- switch.go
+++ switch.go
@@ -3,24 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	day := 1
+	day := 13
 
 	switch day {
-	case 1:
-		fmt.Println("Senin")
-	case 2:
-		fmt.Println("Selasa")
-	case 3:
-		fmt.Println("Rabu")
-	case 4:
-		fmt.Println("Kamis")
-	case 5:
-		fmt.Println("Jumat")
-	case 6:
-		fmt.Println("Sabtu")
-	case 7:
-		fmt.Println("Minggu")
+	case 1, 2, 3, 4, 5:
+		fmt.Println("Weekdays")
+	case 6, 7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Nothing days")
 
 	}
+
+	// switch day {
+	// case 1:
+	// 	fmt.Println("Senin")
+	// case 2:
+	// 	fmt.Println("Selasa")
+	// case 3:
+	// 	fmt.Println("Rabu")
+	// case 4:
+	// 	fmt.Println("Kamis")
+	// case 5:
+	// 	fmt.Println("Jumat")
+	// case 6:
+	// 	fmt.Println("Sabtu")
+	// case 7:
+	// 	fmt.Println("Minggu")
+
+	// }
 
 }
