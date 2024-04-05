726f5efcbf31d00308f58c8d67a11cefc042f0ec
septianhari <aribarno86@gmail.com>
2024-04-05 15:00:19 +0700

    menambah konstanta

---
+++ konstanta.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Mendefinisikan konstanta dengan kata kunci "const"
+const (
+	pi       = 3.14
+	language = "Go"
+)
+
+func main() {
+	// Menggunakan konstanta dalam program
+	fmt.Println("Nilai Pi adalah:", pi)
+	fmt.Println("Bahasa pemrograman ini adalah:", language)
+}
