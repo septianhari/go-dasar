2ee9cc607b289298a30c421ad390aaf62b3525f1
septianhari <aribarno86@gmail.com>
2024-04-07 05:36:48 +0700

    cp2 blm selesai

---
+++ latihan-basic.go/cp2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func BMICalculator(gender string, height int,) float64{
+
+	if gender == "laki-laki" {
+}
+
+
+
+func main() {
+	fmt.Println(BMICalculator("laki-laki", 165))
+	fmt.Println(BMICalculator("perempuan", 165))
+
+}
