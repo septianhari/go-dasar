81bee581b470a662f2b55889d8efd38c52d6999e
septianhari <aribarno86@gmail.com>
2024-04-13 22:37:19 +0700

    Pointer masih

--- pointer.go
+++ pointer.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	var number int
-	number = 100
-	var numberPointer *int
-	numberPointer = &number
+func swap(a, b int) {
+	c := a
+	a = b
+	b = c
 
-	fmt.Println(&number)
-	fmt.Println(*numberPointer)
+	fmt.Println("Di dalam swap", a, b)
+}
+
+func main() {
+	x := 1
+	y := 2
+	fmt.Println("Sebelum swap", x, y)
+	swap(x, y)
+	fmt.Println("Setelah swap", x, y)
+
+	// 	var number int
+	// 	number = 100
+	// 	var numberPointer *int
+	// 	numberPointer = &number
+
+	// fmt.Println(&number)
+	// fmt.Println(*numberPointer)
 }
