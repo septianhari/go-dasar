3bd8e076ab97795f5226cce3c5416bd549bbb5cf
septianhari <aribarno86@gmail.com>
2024-04-14 23:37:03 +0700

    array

--- array.go
+++ array.go
@@ -3,18 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	var student1, student2, student3 string
-	student1 = "Har"
-	student2 = "Bobi"
-	student3 = "Rudi"
-
-	fmt.Println(student1, student2, student3)
-
 	var students [3]string
 	students[0] = "Har"
 	students[1] = "Bobi"
 	students[2] = "Rudi"
 
-	fmt.Println(students[0], students[1], students[2])
+	fmt.Println(students)
+
+	// var numbers [6]int (//arr 1 dimensi)
+	// for i := 0; i <= 5; i++ {
+	// 	numbers[i] = i
+	// }
+	// fmt.Println(numbers)
+	//====================================
+	// students := [5]string{"Har", "Bobi", "Rudi"}
+
+	// fmt.Println(students)
+	//====================================
+	// var student1, student2, student3 string
+	// student1 = "Har"
+	// student2 = "Bobi"
+	// student3 = "Rudi"
+
+	// fmt.Println(student1, student2, student3)
+	//======================================
+	// var students [3]string
+	// students[0] = "Har"
+	// students[1] = "Bobi"
+	// students[2] = "Rudi"
+
+	// fmt.Println(students[0], students[1], students[2])
 
 }
