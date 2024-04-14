b1a2466664a77ba817dab49aec9e9306c6f5f46f
septianhari <aribarno86@gmail.com>
2024-04-14 22:55:45 +0700

    array

--- array.go
+++ array.go
@@ -9,4 +9,12 @@ func main() {
 	student3 = "Rudi"
 
 	fmt.Println(student1, student2, student3)
+
+	var students [3]string
+	students[0] = "Har"
+	students[1] = "Bobi"
+	students[2] = "Rudi"
+
+	fmt.Println(students[0], students[1], students[2])
+
 }
