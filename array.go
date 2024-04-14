97faf8ebfb2867c2a973aa9d05bd1c0ae1611fc7
septianhari <aribarno86@gmail.com>
2024-04-14 22:41:53 +0700

    array

---
+++ array.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var student1, student2, student3 string
+	student1 = "Har"
+	student2 = "Bobi"
+	student3 = "Rudi"
+
+	fmt.Println(student1, student2, student3)
+}
