b09fc6b8220b01e58a6dbdd01bb6a1d405f3fa18
septianhari <aribarno86@gmail.com>
2024-04-16 19:59:18 +0700

    goroutine

---
+++ goroutine.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("main started")
+
+	go func() {
+		fmt.Println("Hello from new goroutine")
+	}()
+
+	fmt.Println("Before sleep")
+	time.Sleep(10 * time.Millisecond) // menunggu 10 milli-second agar go routine berjalan
+	fmt.Println("main stoped")
+}
