7e71c6e51744c99c002e080790cf7c023c011148
septianhari <aribarno86@gmail.com>
2024-04-27 16:56:35 +0700

    go-slice

--- slice.go
+++ slice.go
@@ -3,8 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	names := [5]string{"Har", "Bobi", "Rudi", "Gibran", "Owi"}
+	//buat slice array
+	// names := [5]string{"Har", "Bobi", "Rudi", "Gibran", "Owi"}
+	// fmt.Println(names)
 
+	// bestNames := names[0:3]
+	// fmt.Println(bestNames)
+
+	names := []string{"Har", "Bobi", "Rudi", "Gibran", "Owi"}
 	fmt.Println(names)
+
+	//pake make
+	newNames := make([]string, 3)
+	newNames[0] = "Har"
+	newNames[1] = "Bobi"
+	newNames[2] = "Rudi"
+	newNames = append(newNames, "Owi")
+	fmt.Println(newNames)
 
 }
