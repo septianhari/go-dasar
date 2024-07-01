ab23900f8d703df202b0f16ab237cf9c92d2d9fd
septianhari <aribarno86@gmail.com>
2024-07-01 22:22:40 +0700

    commit

--- nestedloop.go
+++ nestedloop.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+func People() {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Println(i, j)
+		}
+	}
+}
+
 func main() {
 	for i := 1; i <= 2; i++ {
 		for j := 0; j < 3; j++ {
