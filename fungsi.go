c8a1a622c7a7b6f29b774a044fbeda4b297ccdfd
3somb <threesomb@gmail.com>
2024-07-31 22:07:53 +0700

    func

--- fungsi.go
+++ fungsi.go
@@ -2,53 +2,23 @@ package main
 
 import "fmt"
 
-//secara basic function = memiliki name-parameter dan return type
-
-func add(a int, b int) int {
-	//fmt.Println(a + b)
-	return a + b
+func getHello(name string) string {
+	return "Hello " + name
 }
 
-func sayHello(name string) {
-	fmt.Printf("Hallo, %s\n", name)
+// return multiple value
+func getFullName(firstName string, lastName string) string {
+	return firstName + " " + lastName
 }
 
-func addNumbers(numbers ...int) int { //varidaic parameter
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
-}
-
-// bagian-defer = mengakhirkan/menunda suatu ekseskusi dari func
-func readFile(fileName string) {
-	fmt.Println("Open file")
-	defer fmt.Println("Close file")
-
-	fileContent := "Hello"
-	fmt.Println(fileContent)
-
-	if fileContent == "Hello" {
-		return
-	}
-}
+// named return values
 
 func main() {
+	result := getHello("Bagas")
+	fmt.Println(result)
 
-	readFile("test.txt")
-
-	// a := 2
-	// b := 6
-	fmt.Println(addNumbers(10, 3)) //variadic parameter
-	fmt.Println(addNumbers(10, 3, 1, 1))
-
-	fmt.Println(add(2, 4))
-	fmt.Println(add(1, 1))
-	fmt.Println(add(6, 2))
-
-	sayHello("moriis")
-	sayHello("John")
-	sayHello("Miu")
-
+	var firstName = "Otong"
+	var lastName = "Surotong"
+	result = getFullName(firstName, lastName)
+	fmt.Println(result)
 }
