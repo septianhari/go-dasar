52c57912dbbc56d32081e22f66b0b746cbcd3ad4
septianhari <aribarno86@gmail.com>
2025-01-29 23:31:16 +0700

    Implement password input validation in main function

--- for.go
+++ for.go
@@ -6,8 +6,22 @@ import (
 
 func main() {
 
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	var correctPassword string = "juned321"
+	var input string
+
+	for input != correctPassword {
+		fmt.Print("Enter password: ")
+		fmt.Scanln(&input)
+
+		if input != correctPassword {
+			fmt.Println("Incorrect password!")
+		}
 	}
+
+	fmt.Println("Correct password!")
 
 }
